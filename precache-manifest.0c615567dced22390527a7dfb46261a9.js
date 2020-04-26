a1ca4b61ee51f106995af7bb1c92cb8b60ba1fc6
Gasparzinho <gaspar.soto@gmail.com>
2020-04-26 00:09:24 -0400

    Updates

---
+++ precache-manifest.0c615567dced22390527a7dfb46261a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4132caba465065db01ab42e56d2a3620",
+    "url": "/Gasperan/json-rule-engine-ui.git/index.html"
+  },
+  {
+    "revision": "56c1fc706c174a77008d",
+    "url": "/Gasperan/json-rule-engine-ui.git/static/css/main.f1a53ea3.chunk.css"
+  },
+  {
+    "revision": "9b1ade273711ba4d84a9",
+    "url": "/Gasperan/json-rule-engine-ui.git/static/js/2.f3daf70d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Gasperan/json-rule-engine-ui.git/static/js/2.f3daf70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c1fc706c174a77008d",
+    "url": "/Gasperan/json-rule-engine-ui.git/static/js/main.55912ff9.chunk.js"
+  },
+  {
+    "revision": "fdab969579bc98381724",
+    "url": "/Gasperan/json-rule-engine-ui.git/static/js/runtime-main.0430b1c8.js"
+  }
+]);
